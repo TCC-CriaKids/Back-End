5644d388f1d1854b2c82ff194e72770213e4bbd2
pauloaug07 <pauloaugustocosta9@gmail.com>
2025-08-21 09:09:56 -0300

    models corrigidos

--- models/crianca.py
+++ models/crianca.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel
-from datetime import datetime
+from pydantic import BaseModel, Field
+from typing import Annotated
+from datetime import date
 
 class Crianca(BaseModel):
-    cpf: str
-    nome: str
-    data_nascimento: datetime
+    cpf: Annotated[str, Field(min_length=11, max_length=11, pattern=r'^\d{11}$')]  # CPF 11 dígitos
+    nome: str  
+    data_nascimento: date  
