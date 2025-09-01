0b8aa306dc5f3c08637254f2eef023f9029e8d21
pauloaug07 <pauloaugustocosta9@gmail.com>
2025-09-01 12:42:51 -0300

    add router criancas no main.py - Outra seção no swagger

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI  # Importa a classe FastAPI
 # from routes.route import router  # Importa as rotas definidas em route.py
-from routes import atividades, responsaveis, respostas, auth, status
+from routes import atividades, responsaveis, respostas, auth, status, criancas
 
 app = FastAPI()  # Cria a aplicação FastAPI
 
 app.include_router(status.router, tags=["Status de conexão com o banco"])
 app.include_router(atividades.router, tags=["Atividades"])
 app.include_router(respostas.router, tags=["Respostas"])
-app.include_router(responsaveis.router, tags=["Responsáveis e crianças"])
+app.include_router(responsaveis.router, tags=["Responsáveis"])
+app.include_router(criancas.router, tags=["Crianças"])
 app.include_router(auth.router, tags=["Autenticação"])
